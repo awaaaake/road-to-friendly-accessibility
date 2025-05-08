4d62a92ced81d3112823d3f7bb10a0147b4b488c
choyunhee <xornd1110@naver.com>
2025-05-08 18:29:32 +0900

    feat: 🎸 앱 진입점에서 스크린리더용 dom 요소들 생성(aria라이브 영역)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,14 @@ import Room from '@/pages/room/Room';
 import { GlobalStyle } from '@/styles/GlobalStyle';
 
 import { ToastProvider } from './contexts';
+import ScreenReaderAnnouncers from './components/ScreenReaderAnnouncers';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <ToastProvider>
+        <ScreenReaderAnnouncers />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/rooms/:roomId" element={<Room />} />
